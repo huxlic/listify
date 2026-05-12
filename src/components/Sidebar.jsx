85c0231f024a4044aa4b775d917c68aba5817bd1
huxlic <oladimejihassan03@gmail.com>
2026-05-12 14:09:37 +0100

    feat: update package.json for gh-pages deployment and add new dependencies; refine Sidebar component layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     const {theme} = useContext(ThemeContext);
 
   return (
-    <aside className="w-[15%] h-max flex p-3 h-max items-center">
+    <aside className="w-[15%] h-max flex p-3 items-center">
       <h1 className={`${theme ? "text-white" : "text-black"} font-black text-[1.3rem]`}>Listify📋</h1>
     </aside>
   );
