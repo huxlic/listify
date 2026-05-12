08c9b9dfcd1a11ba80fcc8838fb746b9c3e9446f
huxlic <oladimejihassan03@gmail.com>
2026-05-12 10:45:29 +0100

    feat: initialize project with React, Vite, and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[#111111] w-screen h-screen box-border flex p-1">
+        <aside className="w-[15%]"></aside>
+        <main className="flex-1 bg-[#171717] rounded-lg border border-[#444]"></main>
+      </div>
+    </>
+  );
+}
+
+export default App
