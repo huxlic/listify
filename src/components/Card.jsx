6730713a77d7b4f86e403a469f349f02b1ee5512
huxlic <oladimejihassan03@gmail.com>
2026-05-12 16:38:59 +0100

    feat: enhance card and column components with task management features and add input form in header

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,33 @@
 // import React from 'react'
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+import { useContext } from "react";
+import { ThemeContext } from "../hooks/GlobalContext";
+import { LuCalendarDays } from "react-icons/lu";
 
-export default Card
+const Card = ({ title, date, creator, bg }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      draggable="true"
+      style={{ backgroundColor: theme ? bg : "white" }}
+      className={`cursor-grab w-full h-max font-sans rounded-lg flex flex-col p-2 gap-2 border ${theme ? "border-[#333]" : "border-[#E7E7E9]"}`}
+    >
+      <span className="text-[#9CA3AF] text-[.65rem] font-medium]">
+        {creator}
+      </span>
+
+      <p className="text-[.7rem]">{title}</p>
+      {date && (
+        <div className="flex items-center gap-1 text-[.65rem]">
+          <LuCalendarDays color={theme ? "#aaa" : "#9CA3AF"} />
+          <time className="text-[#9CA3AF] font-medium" dateTime="">
+            {date}
+          </time>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
